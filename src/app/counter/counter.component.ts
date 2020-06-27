29742a5f11d4b275e6ce54e7de201bfa7da28ad5
Alexandr Vetrov <a.vetrov@ispsystem.com>
2020-06-27 23:42:10 +0800

    feat: add counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CounterComponent {
+  value = 0;
+}
